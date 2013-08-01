5e06daf9fc1c41576947d1d345ef50ca16a4747d
Amr Gawish <amr.gawish@gmail.com>
2013-08-01 18:30:55 +0100

    Start JDeveloper with JDEV_USER_HOME without the need to mess with old JDeveloper versions

--- Chapter 1/startJDeveloper.bat
+++ Chapter 1/startJDeveloper.bat
@@ -1,3 +1,4 @@
 set JDEV_USER_HOME=%WORK_HOME%\adf
 
-start %APPS_HOME%\oracle\Middleware\Oracle_Home\jdeveloper\jdev\bin\jdev.exe 
+start %APPS_HOME%\oracle\Middleware\Oracle_Home\jdeveloper\jdev\bin\jdev.exe
+
