8cbc5a7a3b5b02a3ba97b905897ab8c60e04566e
Amr Gawish <amr.gawish@gmail.com>
2014-01-09 15:06:27 +0000

    Update EventsBean.java

--- Chapter 7/Starter Applications/3.ADFFacesClientEvents/ViewController/src/com/adffaces/chapter7/view/beans/EventsBean.java
+++ Chapter 7/Starter Applications/3.ADFFacesClientEvents/ViewController/src/com/adffaces/chapter7/view/beans/EventsBean.java
@@ -27,29 +27,4 @@ public class EventsBean {
         ((RichButton) actionEvent.getSource()).setText("Submitted");
 
     }
-
-    public void changeServerEmail(ClientEvent clientEvent) {
-        // Add event code here...
-        Object oldValue = clientEvent.getParameters().get("oldValue");
-        Object newValue = clientEvent.getParameters().get("newValue");
-        System.out.println("Email Changed from: " + oldValue + " to: " + newValue);
-    }
-
-    public void changePhoneNumber(ValueChangeEvent valueChangeEvent) {
-        // Add event code here...
-        String newValue = valueChangeEvent.getNewValue().toString();
-        if (!newValue.matches("^\\d.*$")) {
-            FacesContext context = FacesContext.getCurrentInstance();
-            ExtendedRenderKitService erks = Service.getRenderKitService(context, ExtendedRenderKitService.class);
-            StringBuilder builder = new StringBuilder();
-            builder.append("var component = AdfPage.PAGE.findComponentByAbsoluteId('" +
-                           ((RichInputText) valueChangeEvent.getSource()).getClientId() + "');");
-            builder.append("var message=new AdfFacesMessage(AdfFacesMessage.TYPE_WARNING, 'Double Check your input', 'Are you sure you want to enter " +
-                           newValue + " as a Phone Number');");
-            builder.append("AdfPage.PAGE.addMessage(component.getClientId(),message);");
-            builder.append("AdfPage.PAGE.showMessages(component.getClientId());");
-            builder.append("component.focus();");
-            erks.addScript(context, builder.toString());
-        }
-    }
 }
